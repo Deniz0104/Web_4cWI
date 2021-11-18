64bb9028d7212333658799afac6f95f9f1f29a23
Deniz <deniz.karakoc0104@gmail.com>
2021-11-18 09:59:42 +0100

    hi

--- Javascript/ToDo List/script.js
+++ Javascript/ToDo List/script.js
@@ -1,9 +1,21 @@
-
+var taskArray = [];
 function addTask() {
     var todoList = document.getElementById('todoList');
     var input = document.getElementById("inputTask").value;
     
-    todoList.innerHTML += "<li class='todotasks'>"+ input +"</li>";
+    todoList.innerHTML += "<li class='todotasks"+taskArray.length+"'>"+ input +"</li>";
+}
+
+
+function drawTasks() {
+    for (let i = 0; i < taskArray.length; i++) {
+        todoList.innerHTML = "Hello" + taskArray[i];
+        
+        
+    }
+
+    
+
 }
 
 function deleteTask() {
