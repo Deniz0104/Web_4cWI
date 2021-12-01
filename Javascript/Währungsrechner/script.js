ba18e30c0626b5b0c547682c6a2ef8f7506c9f8a
Deniz <deniz.karakoc0104@gmail.com>
2021-12-01 10:02:56 +0100

    update

---
+++ Javascript/Währungsrechner/script.js
@@ -0,0 +1,6 @@
+var currencyValue = document.getElementById("currencies").value;
+var currencyCalculate = document.getElementById("calculatedCurrency").value;
+
+function calculation() {
+    var inputValue = document.getElementById("inputTask").value;
+}
