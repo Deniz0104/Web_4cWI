822a596cedf192620ef09b13fe4504c681ddb563
Deniz <deniz.karakoc0104@gmail.com>
2021-12-02 11:11:58 +0100

    started ToDo_REST

--- Javascript/Währungsrechner/script.js
+++ Javascript/Währungsrechner/script.js
@@ -1,6 +1,33 @@
-var currencyValue = document.getElementById("currencies").value;
-var currencyCalculate = document.getElementById("calculatedCurrency").value;
+
+var output = document.getElementById("resultarea");
 
 function calculation() {
     var inputValue = document.getElementById("inputTask").value;
+    var currency1 = document.getElementById("firstCurrencies").value;
+    var currency2 = document.getElementById("secondCurrencies").value;
+    var output = document.getElementById("resultarea");
+    let result = ((inputValue / setRate(currency1)) * setRate(currency2));
+    
+    output.innerHTML = result + " " + currency2;
+
 }
+
+function setRate(currency) {
+    let rate = {
+        "Euro": 1,
+        "USD": 1.13,
+        "BitCoin": 0.000024,
+    };
+    switch(currency) {
+        case "Euro":
+            return rate.Euro;
+        case "USD":
+            return rate.USD;
+        case "Bitcoins":
+            return rate.BitCoin;        
+    }
+}
+
+
+
+
