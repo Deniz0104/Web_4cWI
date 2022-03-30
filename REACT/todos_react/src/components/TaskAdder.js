81648378666e0408aeeb26d398e8ae23edf45b5a
Deniz <deniz.karakoc0104@gmail.com>
2022-03-30 10:25:27 +0200

    testing netlify changes

--- REACT/todos_react/src/components/TaskAdder.js
+++ REACT/todos_react/src/components/TaskAdder.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
+import Tasks from './Tasks'
 
 export default class TaskAdder extends Component {
 
 
     addTask = () => {
         alert("Task added!")
+        let newTask;
+
+    }
+
+    handleChange = ()=> {
+      console.log("changing")
+    }
+
+    value = {
+        "id": null,
+        "name": newTask,
+        "isDone": false
     }
 
   render() {
     return (
       <div>
-          <input placeholder='Add a task ...'></input>
+          <input placeholder='Add a task ...' onChange={handleChange}></input>
           <button onClick={this.addTask}>Add</button>
       </div>
     )
